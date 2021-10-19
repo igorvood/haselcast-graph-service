6ac0591238176f1152a6fc06e9779f8a22469b7c
vood <balitsky_igor@list.ru>
2021-10-19 19:28:29 +0300

    Initial commit

---
+++ src/main/kotlin/ru/vood/graph/haselcastgraphservice/HaselcastGraphServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.vood.graph.haselcastgraphservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HaselcastGraphServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<HaselcastGraphServiceApplication>(*args)
+}
